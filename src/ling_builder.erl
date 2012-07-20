68ee39a64b4ebb44fdd915ef34e56cacf29a8c0b
Maxim Kharchenko <maxim.kharchenko@gmail.com>
2012-07-21 02:05:38 +0300

    first successful Ling image build

--- src/ling_builder.erl
+++ src/ling_builder.erl
@@ -1,8 +1,83 @@
 -module(ling_builder).
 
--export(['ling-build'/2]).
+-export(['ling-build'/2,'ling-image'/2]).
 
-'ling-build'(Config, AppFile) ->
-	io:format("ling-build: Config ~p AppFile ~p~n", [Config, AppFile]).
+'ling-build'(Config, _AppFile) ->
+
+	case whereis(ling_queue) of
+	undefined ->
+		ling_queue:start_link();
+	_ ->
+		ok
+	end,
+
+	BaseDir = rebar_config:get_global(base_dir, undefined),
+	IsBaseDir = rebar_utils:get_cwd() =:= BaseDir,
+
+	IsPluginDir = filename:basename(rebar_utils:get_cwd()) =:= "ling_builder",
+
+	if not IsPluginDir ->
+
+		Files = [filename:absname(F) || F <- filelib:wildcard("ebin/*")],
+		ling_queue:add(Files);
+
+	true ->
+		skip
+	end,
+
+	if IsBaseDir ->
+
+		ProjName = filename:basename(BaseDir),
+		LingOpts = rebar_config:get(Config, ling_opts, []),
+		ling_queue:start_build(ProjName, LingOpts);
+
+	true ->
+		skip
+	end,
+
+	ok.
+
+'ling-image'(Config, _AppFile) ->
+
+	BaseDir = rebar_config:get_global(base_dir, undefined),
+	IsBaseDir = rebar_utils:get_cwd() =:= BaseDir,
+
+	if not IsBaseDir ->
+		ok;
+	true ->
+
+		rebar_log:log(info, "starting inets~n", []),
+		application:start(crypto),
+		application:start(public_key),
+		application:start(ssl),
+		application:start(inets),
+
+		ProjName = filename:basename(BaseDir),
+		LingOpts = rebar_config:get(Config, ling_opts, []),
+
+		{_,BuildHost} = lists:keyfind(build_host, 1, LingOpts),
+		{_,UserName} = lists:keyfind(username, 1, LingOpts),
+		{_,Password} = lists:keyfind(password, 1, LingOpts),
+
+		Encoded = base64:encode_to_string(lists:append([UserName,":",Password])),
+		AuthHeader = {"Authorization","Basic " ++ Encoded},
+		Headers = [AuthHeader],
+
+		ImageFile = "vmling-" ++ ProjName,
+
+		Location = "https://" ++ BuildHost ++ "/1/build/" ++ ProjName,
+		case httpc:request(get, {Location,Headers}, [], []) of
+		{ok,{{_,200,_},_,RespBody}} ->
+			ImageBin = list_to_binary(RespBody),
+			rebar_log:log(info, "saving image to ~s [~w byte(s)]~n",
+									[ImageFile,byte_size(ImageBin)]),
+			ok = file:write_file(ImageFile, ImageBin),
+			io:format("Image saved to ~s~n", [ImageFile]);
+		{ok,{{_,404,_},_,_}} ->
+			io:format("Image is not (yet) available~n", [])
+		end,
+
+		ok
+	end.
 
 %%EOF
---
+++ src/ling_queue.erl
@@ -0,0 +1,133 @@
+-module(ling_queue).
+-behaviour(gen_server).
+-define(SERVER, ?MODULE).
+
+%% ------------------------------------------------------------------
+%% API Function Exports
+%% ------------------------------------------------------------------
+
+-export([start_link/0]).
+
+-export([add/1]).
+-export([start_build/2]).
+
+%% ------------------------------------------------------------------
+%% gen_server Function Exports
+%% ------------------------------------------------------------------
+
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+         terminate/2, code_change/3]).
+
+%% ------------------------------------------------------------------
+%% API Function Definitions
+%% ------------------------------------------------------------------
+
+start_link() ->
+    gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+
+add(Files) ->
+	gen_server:call(?SERVER, {add,Files}).
+
+start_build(ProjName, LingOpts) ->
+	gen_server:call(?SERVER, {start_build,ProjName,LingOpts}).
+
+%% ------------------------------------------------------------------
+%% gen_server Function Definitions
+%% ------------------------------------------------------------------
+
+init(_Args) ->
+	{ok, []}.
+
+handle_call({add,More}, _From, Files) ->
+    {reply, ok, More ++ Files};
+
+handle_call({start_build,ProjName,LingOpts}, _From, Files) ->
+
+	rebar_log:log(info, "starting inets~n", []),
+	application:start(crypto),
+	application:start(public_key),
+	application:start(ssl),
+	application:start(inets),
+
+	AH = {"Accept","text/plain"},
+	case call_lbs(get, "/projects/" ++ ProjName, [AH], none, LingOpts) of
+	{ok,Body} ->
+		RemoteFiles = string:tokens(Body, "|"),
+
+		FileNames = [filename:basename(F) || F <- Files],
+		DeleteUs = lists:filter(fun(F) ->
+				not lists:member(F, FileNames)
+			end, RemoteFiles),
+
+		lists:foreach(fun(F) ->
+				rebar_log:log(info, "delete stale ~s~n", [F]),
+				Slug = "/projects/" ++ ProjName ++ "/" ++ F,
+				ok = call_lbs(delete, Slug, [], none, LingOpts)
+			end, DeleteUs);
+	
+	not_found ->
+		ok = call_lbs(put, "/projects/" ++ ProjName, [], [], LingOpts)
+	end,
+
+	lists:foreach(fun(F) ->
+		FileName = filename:basename(F),
+		{ok,Bin} = file:read_file(F),
+		rebar_log:log(info, "upload ~s [~w byte(s)]~n",
+								[FileName,byte_size(Bin)]),
+		Slug = "/projects/" ++ ProjName ++ "/" ++ FileName,
+		ok = call_lbs(put, Slug, [], Bin, LingOpts)
+	end, Files),
+
+	rebar_log:log(info, "build started for '~s'~n", [ProjName]),
+	{ok,Banner} = call_lbs(post, "/build/" ++ ProjName, [], [], LingOpts),
+	io:format("~s - use 'rebar ling-image' to retrieve~n", [Banner]),
+	
+	{reply,ok,[]}.
+
+handle_cast(_Msg, State) ->
+    {noreply, State}.
+
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+terminate(_Reason, _State) ->
+    ok.
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+%% ------------------------------------------------------------------
+%% Internal Function Definitions
+%% ------------------------------------------------------------------
+
+call_lbs(Method, Slug, Hdrs, Body, LingOpts) ->
+	{_,BuildHost} = lists:keyfind(build_host, 1, LingOpts),
+	{_,UserName} = lists:keyfind(username, 1, LingOpts),
+	{_,Password} = lists:keyfind(password, 1, LingOpts),
+
+	Encoded = base64:encode_to_string(lists:append([UserName,":",Password])),
+	AuthHeader = {"Authorization","Basic " ++ Encoded},
+	Headers = [AuthHeader] ++ Hdrs,
+
+	Location = "https://" ++ BuildHost ++ "/1" ++ Slug,
+	Request = if Body =:= none ->
+			{Location,Headers};
+		true ->
+			{Location,Headers,"application/octet-stream",Body}
+		end,
+
+	case httpc:request(Method, Request, [], []) of
+	{ok,{{_,200,_},_,RespBody}} -> 
+		{ok,RespBody};
+
+	{ok,{{_,204,_},_,_}} ->
+		ok;
+	
+	{ok,{{_,404,_},_,_}} ->
+		not_found;
+
+	Other ->
+		Other
+	end.
+
+%%EOF
