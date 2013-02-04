79e7694afe4a0477fc1a30ae4e8b6d4b1d6f97cf
mk <mk@dev.(none)>
2013-02-04 06:48:00 -0500

    default value for build_host added

--- src/build_service.erl
+++ src/build_service.erl
@@ -2,9 +2,10 @@
 -export([call/5]).
 
 call(Method, Slug, Hdrs, Body, ConnOpts) ->
-	{_,BuildHost} = lists:keyfind(build_host, 1, ConnOpts),
-	{_,UserName} = lists:keyfind(username, 1, ConnOpts),
-	{_,Password} = lists:keyfind(password, 1, ConnOpts),
+	BuildHost = proplists:get_value(build_host,
+			ConnOpts, "build.erlangonxen.org:8080"),
+	UserName = proplists:get_value(username, ConnOpts),
+	Password = proplists:get_value(password, ConnOpts),
 
 	Encoded = base64:encode_to_string(lists:append([UserName,":",Password])),
 	AuthHeader = {"Authorization","Basic " ++ Encoded},
